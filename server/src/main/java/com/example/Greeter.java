bfae8d5b149defb4ba07fe1d6df05f6cb016af69
Fredoninja <53599415+Fredoninja@users.noreply.github.com>
2019-08-02 11:34:56 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,4 +1,4 @@
-package com.example;
+/package com.example;
 
 
 public class Greeter {
@@ -7,7 +7,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
+/**
+* @param someone nema of personn
+* @return greeting string
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
